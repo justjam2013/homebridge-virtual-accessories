dfe094eabbc6579f2915b466628ebc50445c7358
Just Jam <renzo.zanelli6@gmail.com>
2024-11-21 22:31:03 +0000

    Updating current state characteristic

--- src/accessories/virtualAccessoryGarageDoor.ts
+++ src/accessories/virtualAccessoryGarageDoor.ts
@@ -124,7 +124,15 @@ export class GarageDoor extends Accessory {
       this.saveState(this.storagePath, this.stateStorageKey, this.states.GarageDoorState);
     }
 
-    this.platform.log.info(`[${this.accessoryConfiguration.accessoryName}] Setting Target Door State: ${this.getStateName(this.states.GarageDoorState)}`);
+    // CurrentDoorState CLOSING/OPENING
+    // Timer
+    // CurrentDoorState OPEN/CLOSED
+    this.service!.setCharacteristic(this.platform.Characteristic.CurrentDoorState, (this.states.GarageDoorState));
+    // eslint-disable-next-line max-len
+    this.platform.log.info(`[${this.accessoryConfiguration.accessoryName}] Setting Current Garage Door State: ${this.getStateName(this.states.GarageDoorState)}`);
+
+    // eslint-disable-next-line max-len
+    this.platform.log.info(`[${this.accessoryConfiguration.accessoryName}] Setting Target Garage Door State: ${this.getStateName(this.states.GarageDoorState)}`);
   }
 
   /**
