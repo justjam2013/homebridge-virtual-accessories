9cc5df6f112a5ffb3eee1a095cd3190ba788976e
Just Jam <renzo.zanelli6@gmail.com>
2025-03-15 14:13:21 +0000

    Added log muting for Switch accessory

--- src/configuration/configurationSwitch.ts
+++ src/configuration/configurationSwitch.ts
@@ -7,6 +7,7 @@ export class SwitchConfiguration {
   defaultState!: string;
   hasResetTimer: boolean = false;
   hasCompanionSensor: boolean = false;
+  muteLogging: boolean = false;
 
   static prefix: string = 'switch';
 
